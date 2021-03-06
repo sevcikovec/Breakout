fef7226e35269dc2ad68bc63813159fd8b727445
Jan Ševčík <sevcikovec@gmail.com>
2021-03-06 09:21:52 +0100

    Added color animation of test quad.

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "lodepng.h"
 #include "main.h"
 #include "Renderer/Shader.h"
+#include "Math/Vec3.h"
 
 using namespace std;
 
@@ -48,17 +49,27 @@ int main(void)
     const char* fragmentShader = "..\\..\\..\\..\\resources\\shaders\\frag.glsl";
     Shader testShader(vertexShader, fragmentShader);
 
-   
+    Vec3 color(1, 0, 0);
+    double startTime = glfwGetTime();
+    double currentTime = startTime;
 
+    double frameTime = 0;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        frameTime = glfwGetTime() - currentTime;
+        currentTime = glfwGetTime();
+
+        // animate color
+        color.x = sin(currentTime) / 2.f + .5f;
+        color.y = cos(currentTime) / 2.f + .5f;
+
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
         // test shader uniform binding
         testShader.Bind();
-        testShader.SetUniformFloat4("color", 0.0f, 0.5f, 0.8f, 1);
+        testShader.SetUniformFloat4("color", color.x, color.y, color.z, 1);
         
         glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
